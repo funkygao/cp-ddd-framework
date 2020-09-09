99d468794c97700f5162148402a7a7a8e20a158a
gaopeng71 <gaopeng8@jd.com>
2020-09-09 13:37:52 +0800

    demonstrate how to inject default extension

--- cp-ddd-example/order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/ability/AssignOrderNoAbility.java
+++ cp-ddd-example/order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/ability/AssignOrderNoAbility.java
@@ -1,15 +1,20 @@
 package org.example.cp.oms.domain.ability;
 
 import org.example.cp.oms.domain.CoreDomain;
+import org.example.cp.oms.domain.ability.extension.DefaultAssignOrderNoExt;
 import org.example.cp.oms.spec.ext.IAssignOrderNoExt;
 import org.example.cp.oms.spec.model.IOrderModel;
 import org.ddd.cp.ddd.annotation.DomainAbility;
 import org.ddd.cp.ddd.model.BaseDomainAbility;
 
+import javax.annotation.Resource;
 import javax.validation.constraints.NotNull;
 
 @DomainAbility(domain = CoreDomain.CODE, name = "分配订单号的能力")
 public class AssignOrderNoAbility extends BaseDomainAbility<IOrderModel, IAssignOrderNoExt> {
+
+    @Resource
+    private DefaultAssignOrderNoExt defaultAssignOrderNoExt;
 
     public void assignOrderNo(@NotNull IOrderModel model) {
         firstExtension(model).assignOrderNo(model);
@@ -17,7 +22,6 @@ public class AssignOrderNoAbility extends BaseDomainAbility<IOrderModel, IAssign
 
     @Override
     public IAssignOrderNoExt defaultExtension(@NotNull IOrderModel model) {
-        // 没有默认的扩展点实现
-        return null;
+        return defaultAssignOrderNoExt;
     }
 }
