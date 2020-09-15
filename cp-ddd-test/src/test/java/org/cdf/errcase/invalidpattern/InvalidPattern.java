2924d33760b9c7d86a6079090a68629792eb045e
gaopeng71 <gaopeng8@jd.com>
2020-09-15 14:46:57 +0800

    explains

--- cp-ddd-test/src/test/java/org/cdf/errcase/invalidpattern/InvalidPattern.java
+++ cp-ddd-test/src/test/java/org/cdf/errcase/invalidpattern/InvalidPattern.java
@@ -2,6 +2,7 @@ package org.cdf.errcase.invalidpattern;
 
 import org.cdf.ddd.annotation.Pattern;
 
+// 该Pattern故意没有实现IIdentityResolver
 @Pattern(code = InvalidPattern.CODE, name = "B2B模式", priority = 90)
 public class InvalidPattern {
     public static final String CODE = "b2b";
