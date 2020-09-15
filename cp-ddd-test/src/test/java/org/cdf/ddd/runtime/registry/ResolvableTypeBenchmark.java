ba61e7164dcc2c6b243dd3d90c889c0d596f7e72
gaopeng71 <gaopeng8@jd.com>
2020-09-15 09:38:42 +0800

    更真实的benchmark test

--- cp-ddd-test/src/test/java/org/cdf/ddd/runtime/registry/ResolvableTypeBenchmark.java
+++ cp-ddd-test/src/test/java/org/cdf/ddd/runtime/registry/ResolvableTypeBenchmark.java
@@ -3,28 +3,31 @@ package org.cdf.ddd.runtime.registry;
 import com.google.caliper.Benchmark;
 import com.google.caliper.api.VmOptions;
 import com.google.caliper.runner.CaliperMain;
-import org.cdf.ddd.runtime.StepsExecTemplate;
+import org.cdf.ddd.runtime.registry.mock.step.SubmitStepsExec;
 import org.springframework.core.ResolvableType;
 
 @VmOptions({"-Xms4g", "-Xmx4g", "-XX:-TieredCompilation"})
 public class ResolvableTypeBenchmark {
-    
+
     public static void main(String[] args) {
         CaliperMain.main(ResolvableTypeBenchmark.class, args);
     }
 
-    // 400 ns
+    // 300 ns
     @Benchmark
     public void stepExecTemplateResolveStepExType(int N) throws Exception {
+        ChildStepsExec stepsExec = new ChildStepsExec();
         for (int i = 0; i < N; i++) {
-            resolveStepEx();
+            stepsExec.resolve();
         }
     }
 
-    private void resolveStepEx() {
-        ResolvableType t = ResolvableType.forClass(StepsExecTemplate.class);
-        ResolvableType stepType = t.getGeneric(0);
-        ResolvableType stepExceptionType = stepType.getGeneric(1);
-        stepExceptionType.resolve();
+    static class ChildStepsExec extends SubmitStepsExec {
+        Class resolve() {
+            ResolvableType stepsExecTemplateT = ResolvableType.forClass(this.getClass());
+            ResolvableType stepT = stepsExecTemplateT.getSuperType().getSuperType().getGeneric(0); // 父类的父类，才能拿到泛型声明
+            Class stepExType = stepT.getInterfaces()[0].getGeneric(1).resolve(); // will be FooException
+            return stepExType;
+        }
     }
 }
