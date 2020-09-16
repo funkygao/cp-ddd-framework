cbd7f1ec2c51ef77daadc0fa190b0e316ca87a93
gaopeng71 <gaopeng8@jd.com>
2020-09-16 08:35:20 +0800

    以不变应万变

--- cp-ddd-spec/src/main/java/org/cdf/ddd/ext/IDomainExtension.java
+++ cp-ddd-spec/src/main/java/org/cdf/ddd/ext/IDomainExtension.java
@@ -6,7 +6,7 @@
 package org.cdf.ddd.ext;
 
 /**
- * 扩展点：业务语义确定，但执行逻辑不同的业务功能点.
+ * 扩展点：业务语义确定，但执行逻辑不同的业务功能点，即以不变应万变.
  * <p>
  * <p>ATTENTION: 扩展点方法的返回值，必须是Java类，而不能是int/boolean等primitive types，否则可能抛出NPE</p>
  */
