ebf362ba446c069668a739dbeea84f3922758e7e
gaopeng71 <gaopeng8@jd.com>
2020-09-18 09:53:41 +0800

    nil

--- cp-ddd-runtime/src/main/java/org/cdf/ddd/model/NamedThreadFactory.java
+++ cp-ddd-runtime/src/main/java/org/cdf/ddd/model/NamedThreadFactory.java
@@ -30,7 +30,6 @@ class NamedThreadFactory implements ThreadFactory {
     public Thread newThread(Runnable r) {
         Thread t = new Thread(group, r, namePrefix + threadCount.getAndIncrement(), 0);
         t.setDaemon(daemon);
-
         return t;
     }
 }
