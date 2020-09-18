b831115910d45cab66d938b6f04c9c5a5792d540
gaopeng71 <gaopeng8@jd.com>
2020-09-18 15:09:38 +0800

    IContainerContext暂时只是预留的，还没想好输出什么能力

--- cp-ddd-spec/src/main/java/org/cdf/ddd/plugin/IContainerContext.java
+++ cp-ddd-spec/src/main/java/org/cdf/ddd/plugin/IContainerContext.java
@@ -5,29 +5,9 @@
  */
 package org.cdf.ddd.plugin;
 
-import java.lang.annotation.Annotation;
-
 /**
  * 容器提供给插件的上下文信息.
  */
 public interface IContainerContext {
-
-    /**
-     * 注册.
-     *
-     * @param annotation 注解类型，目前只支持{@code Partner, Pattern, Extension}
-     * @param object     对象实例
-     * @throws Exception
-     */
-    void registerBean(Class<? extends Annotation> annotation, Object object) throws Exception;
-
-    /**
-     * 注销.
-     *
-     * @param annotation 注解类型，目前只支持{@code Partner, Pattern, Extension}
-     * @param object     对象实例
-     * @throws Exception
-     */
-    void deregisterBean(Class<? extends Annotation> annotation, Object object) throws Exception;
 
 }
