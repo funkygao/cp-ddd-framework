e70e9e703840f8dc1d311b8b2acee6cbf2fa1864
gaopeng71 <gaopeng8@jd.com>
2020-09-22 15:26:25 +0800

    isv再多实现一个扩展点，这样容易测试

--- cp-ddd-example/order-center-bp-isv/src/main/java/org/example/bp/oms/isv/extension/PresortExt.java
+++ cp-ddd-example/order-center-bp-isv/src/main/java/org/example/bp/oms/isv/extension/PresortExt.java
@@ -1,4 +1,19 @@
 package org.example.bp.oms.isv.extension;
 
-public class PresortExt {
+import lombok.extern.slf4j.Slf4j;
+import org.cdf.ddd.annotation.Extension;
+import org.example.bp.oms.isv.IsvPartner;
+import org.example.cp.oms.spec.ext.IPresortExt;
+import org.example.cp.oms.spec.model.IOrderModel;
+
+import javax.validation.constraints.NotNull;
+
+@Extension(code = IsvPartner.CODE, value = "isvPresort")
+@Slf4j
+public class PresortExt implements IPresortExt {
+
+    @Override
+    public void presort(@NotNull IOrderModel model) {
+        log.info("ISV里预分拣的结果：1");
+    }
 }
