cff214979e6b673ebe9aedc33a51dfdd92cafaa1
funkygao <funky.gao@gmail.com>
2020-09-28 23:03:32 +0800

    fix demo bug

--- cp-ddd-example/order-center-bp-isv/src/main/java/org/example/bp/oms/isv/PluginListener.java
+++ cp-ddd-example/order-center-bp-isv/src/main/java/org/example/bp/oms/isv/PluginListener.java
@@ -1,14 +1,8 @@
 package org.example.bp.oms.isv;
 
 import lombok.extern.slf4j.Slf4j;
-import org.cdf.ddd.annotation.Extension;
 import org.cdf.ddd.plugin.IContainerContext;
 import org.cdf.ddd.plugin.IPluginListener;
-import org.example.cp.oms.spec.ext.ISensitiveWordsExt;
-import org.example.cp.oms.spec.model.IOrderModel;
-import org.example.cp.oms.spec.partner.KaPartner;
-
-import javax.validation.constraints.NotNull;
 
 @Slf4j
 public class PluginListener implements IPluginListener {
@@ -21,14 +15,5 @@ public class PluginListener implements IPluginListener {
     @Override
     public void beforeUnload(IContainerContext ctx) throws Exception {
 
-    }
-
-    @Extension(code = KaPartner.CODE)
-    public static class SensitiveWordsExt implements ISensitiveWordsExt {
-
-        @Override
-        public String[] extract(@NotNull IOrderModel model) {
-            return new String[0];
-        }
     }
 }
