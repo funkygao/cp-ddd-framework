0feb8ae0d48c80c2a6523e907c40a51fff7250cf
Funky Gao <funky.gao@gmail.com>
2020-10-01 18:51:46 +0800

    fix the test case

--- cp-ddd-test/src/test/java/org/cdf/ddd/runtime/registry/RegistryFactoryTest.java
+++ cp-ddd-test/src/test/java/org/cdf/ddd/runtime/registry/RegistryFactoryTest.java
@@ -2,12 +2,17 @@ package org.cdf.ddd.runtime.registry;
 
 import org.cdf.ddd.runtime.registry.mock.extension.B2BExt;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring-test.xml"})
 public class RegistryFactoryTest {
 
     @Test
