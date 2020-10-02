d92fb1563ecd18769dd8c98ea8b9bbb8112f1d82
Funky Gao <funky.gao@gmail.com>
2020-10-02 13:58:50 +0800

    stateful LogAssert ok now

--- demo/order-center-cp/cp-oc-test/src/test/java/org/example/cp/oms/LogAssert.java
+++ demo/order-center-cp/cp-oc-test/src/test/java/org/example/cp/oms/LogAssert.java
@@ -1,9 +1,8 @@
 package org.example.cp.oms;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 
 import static org.junit.Assert.fail;
@@ -11,20 +10,26 @@ import static org.junit.Assert.fail;
 class LogAssert {
     private static final String logFile = "logs/app.log";
 
-    private static Scanner scanner = null;
+    private static RandomAccessFile reader = null;
 
-    static void assertContains(String... events) throws FileNotFoundException {
-        if (scanner == null) {
-            scanner = new Scanner(new FileInputStream(logFile));
-        }
-
+    static void assertContains(String... events) throws IOException {
         Set<String> expectedEvents = new HashSet<>(events.length);
         for (String event : events) {
             expectedEvents.add(event);
         }
         Set<String> foundEvents = new HashSet<>(events.length);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
+
+        if (reader == null) {
+            reader = new RandomAccessFile(logFile, "r");
+        }
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                // EOF
+                break;
+            }
+
+            line = new String(line.getBytes("ISO-8859-1"), "utf-8");
             for (String event : events) {
                 if (line.contains(event)) {
                     foundEvents.add(event);
@@ -32,6 +37,7 @@ class LogAssert {
             }
         }
 
+        // 差集
         expectedEvents.removeAll(foundEvents);
         if (!expectedEvents.isEmpty()) {
             fail(String.format("%d events not found: %s", expectedEvents.size(), expectedEvents.toString()));
