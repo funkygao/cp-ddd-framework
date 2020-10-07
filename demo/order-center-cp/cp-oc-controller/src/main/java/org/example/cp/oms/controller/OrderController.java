58bed44d38d8962f8c1290ada85db38febe7a0ca
Funky Gao <funky.gao@gmail.com>
2020-10-07 19:47:47 +0800

    the demo controller can switch between isv and ka mode

--- demo/order-center-cp/cp-oc-controller/src/main/java/org/example/cp/oms/controller/OrderController.java
+++ demo/order-center-cp/cp-oc-controller/src/main/java/org/example/cp/oms/controller/OrderController.java
@@ -7,10 +7,7 @@ import org.example.cp.oms.domain.model.OrderModel;
 import org.example.cp.oms.domain.model.OrderModelCreator;
 import org.example.cp.oms.domain.service.SubmitOrder;
 import org.slf4j.MDC;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -25,35 +22,49 @@ public class OrderController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello() {
+        MDC.put("tid", String.valueOf(System.nanoTime()));
+
         return "Hello cp-ddd-framework!";
     }
 
     // 下单服务
     @RequestMapping(value = "/order", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public String submitOrder() {
+    public String submitOrder(@RequestParam(required = false) String type) {
+        log.info("type={}", type);
+        if (type == null) {
+            type = "ISV"; // ISV by default
+        }
+
         // DTO 转换为 domain model，通过creator保护、封装domain model
         // 具体项目使用MapStruct会更方便，这里为了演示，全手工进行对象转换了
         RequestProfile requestProfile = new RequestProfile();
         requestProfile.setTraceId(String.valueOf(System.nanoTime()));
+        // 演示个性化字段：站点联系人号码是ISV前台场景才会需要的字段，其他场景不需要
+        requestProfile.getExt().put("_station_contact_", "139100988343");
         MDC.put("tid", requestProfile.getTraceId()); // session scope log identifier
 
         // 这里手工创建一个模拟下单的请求
         OrderModelCreator creator = new OrderModelCreator();
         creator.setRequestProfile(requestProfile);
-        creator.setSource("ISV");
+        creator.setSource(type);
         creator.setCustomerNo("home");
-        creator.setExternalNo("2020");
+        creator.setExternalNo("20200987655");
         OrderModel model = OrderModel.createWith(creator);
 
         // 调用domain service完成该use case
         submitOrder.submit(model);
+        // ISV业务前台的下单执行：
+        //   SerializableIsolationExt -> DecideStepsExt -> BasicStep(PresortExt) -> PersistStep(AssignOrderNoExt, CustomModelAbility) -> BroadcastStep
+        // 查看日志，了解具体执行情况
         return "Order accepted!";
     }
 
     @RequestMapping(value = "/reloadIsv")
     @ResponseBody
     public String reloadIsv() {
+        MDC.put("tid", String.valueOf(System.nanoTime()));
+
         log.info("CWD:{}", System.getProperty("user.dir"));
 
         try {
@@ -63,6 +74,6 @@ public class OrderController {
             return cause.getMessage();
         }
 
-        return "Reloaded!";
+        return "ISV Plugin Reloaded!";
     }
 }
