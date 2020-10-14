ccf194539f23f6c59cbc9b2ea07f5b1e94cee614
Funky Gao <funky.gao@gmail.com>
2020-10-14 14:19:41 +0800

    explicit PluginApplicationContext, and add test for ContainerContext

---
+++ dddplus-plugin/src/main/java/io/github/dddplus/runtime/registry/PluginApplicationContext.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright cp-ddd-framework Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.github.dddplus.runtime.registry;
+
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// 每个Plugin特有的Spring application context，使用Plugin特有的class loader
+class PluginApplicationContext extends ClassPathXmlApplicationContext {
+    private final ClassLoader pluginClassLoader;
+
+    PluginApplicationContext(String[] configLocations, ApplicationContext parent, ClassLoader pluginClassLoader) {
+        super(configLocations, false, parent); // will not refresh
+        this.pluginClassLoader = pluginClassLoader;
+    }
+
+    @Override
+    protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
+        super.initBeanDefinitionReader(reader);
+        reader.setBeanClassLoader(pluginClassLoader);
+        setClassLoader(pluginClassLoader); // so that it can find the pluginXml within the jar
+    }
+}
--- dddplus-spec/src/main/java/io/github/dddplus/plugin/IPlugin.java
+++ dddplus-spec/src/main/java/io/github/dddplus/plugin/IPlugin.java
@@ -13,7 +13,13 @@ public interface IPlugin {
     /**
      * 获取插件码.
      * <p>
-     * <p>目前，对应的是{@code Pattern.code} 或 {@code Partner.code}</p>
+     * <p>目前，对应的是{@code Pattern.code} 或 {@code Partner.code}.</p>
+     * <p>Unique within a single JVM.</p>
      */
     String getCode();
+
+    /**
+     * 获取当前版本.
+     */
+    String getVersion();
 }
