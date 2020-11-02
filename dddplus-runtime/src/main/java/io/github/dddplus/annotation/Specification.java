9355912e45f216c3c731762f12c09f76a1e67c57
Funky Gao <funky.gao@gmail.com>
2020-11-02 15:39:42 +0800

    add annotation for ISpecification
    it is a Spring bean, stateless

---
+++ dddplus-runtime/src/main/java/io/github/dddplus/annotation/Specification.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright DDDplus Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.github.dddplus.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * 业务约束规则，注解在{@link io.github.dddplus.specification.ISpecification}之上.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Component
+public @interface Specification {
+
+    /**
+     * 业务约束名称.
+     */
+    String name();
+
+    /**
+     * 该业务约束规则所属标签.
+     * <p>
+     * <p>通过标签，对业务约束规则进行归类</p>
+     */
+    String[] tags() default {};
+
+}
--- dddplus-test/src/test/java/io/github/dddplus/specification/mock/B2CMustHavePartnerCode.java
+++ dddplus-test/src/test/java/io/github/dddplus/specification/mock/B2CMustHavePartnerCode.java
@@ -1,11 +1,13 @@
 package io.github.dddplus.specification.mock;
 
+import io.github.dddplus.annotation.Specification;
 import io.github.dddplus.runtime.registry.mock.model.FooModel;
 import io.github.dddplus.specification.ISpecification;
 import io.github.dddplus.specification.Notification;
 
 import javax.validation.constraints.NotNull;
 
+@Specification(name = "B2C业务必须要传递partnerCode")
 public class B2CMustHavePartnerCode implements ISpecification<FooModel> {
 
     @Override
