1bed7971f2385fbe255b09ba4d2acd72b841c0dd
Funky Gao <funky.gao@gmail.com>
2020-11-24 08:19:11 +0800

    guide given

--- dddplus-test/src/test/java/io/github/dddplus/runtime/registry/mock/policy/TriggerExtPolicy.java
+++ dddplus-test/src/test/java/io/github/dddplus/runtime/registry/mock/policy/TriggerExtPolicy.java
@@ -12,6 +12,7 @@ public class TriggerExtPolicy implements IExtPolicy<FooModel> {
 
     @Override
     public String extensionCode(FooModel model) {
+        // 在此，可以调用配置中心，决定目标扩展点
         if (model.isFoo()) {
             log.info("will use foo");
             return "foo";
