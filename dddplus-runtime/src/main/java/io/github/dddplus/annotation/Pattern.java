a7660a77728fc651329a44d3a857deb029bbb77e
gaopeng71 <gaopeng8@jd.com>
2023-03-20 14:47:33 +0800

    how to understand Pattern

--- dddplus-runtime/src/main/java/io/github/dddplus/annotation/Pattern.java
+++ dddplus-runtime/src/main/java/io/github/dddplus/annotation/Pattern.java
@@ -13,13 +13,15 @@ import java.lang.annotation.*;
 /**
  * 业务模式身份，需要实现{@link IIdentityResolver}接口.
  * <p>
+ * <p>模式，是某种存在于现实事物、人类设计或抽象概念中的规律性；该规律性可被复用</p>
  * <p>{@link Pattern}通常属于中台CP自己要处理的个性化业务，前台BP往往不会参与</p>
  * <p>横向视角把扩展点进行聚合，属于水平业务，可以叠加</p>
  * <p>所谓横向视角，相当于AOP里的Aspect</p>
  * <ul>如何理解{@code Pattern}与{@code IDomainExtension}？
- * <li>{@code Pattern}，相当于把散落在各处的某个业务逻辑{@code if} 判断条件，收敛到{@code Pattern}里，使得这些业务判断显式化，有形化，有了化身，并有了个名字(UL)</li>
+ * <li>{@code Pattern}，相当于把散落在各处的某个业务逻辑{@code if} 判断条件，收敛到{@code Pattern}类里，使得这些业务判断显式化，有形化，有了化身，并有了个名字(UL)</li>
  * <li>扩展点，相当于把{@code if} 后面的code block显式化，有形化</li>
  * </ul>
+ * <p>{@link Pattern}是InnerSource开发模式的有效手段：中台定义模式编码并通过{@link Pattern}统一定义路由，中台与BP共同在一个共享代码库实现{@link Pattern}对应的扩展点.</p>
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
