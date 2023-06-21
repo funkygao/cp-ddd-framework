048f57137447dc0f865adb7457186fade36072f3
Funky Gao <funky.gao@gmail.com>
2023-06-21 23:20:04 +0800

    add dddplus-mybatis module

---
+++ dddplus-mybatis/src/main/java/io/github/dddplus/mybatis/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright DDDplus Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+/**
+ * DDDplus integration with MyBatis support.
+ */
+package io.github.dddplus.mybatis;
