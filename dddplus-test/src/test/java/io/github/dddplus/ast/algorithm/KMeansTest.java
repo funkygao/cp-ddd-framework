62edb59918f9a331e565213f8db66e08a23afd20
gaopeng71 <gaopeng8@jd.com>
2023-06-27 10:16:52 +0800

    try KMeans to cluster method names

---
+++ dddplus-test/src/test/java/io/github/dddplus/ast/algorithm/KMeansTest.java
@@ -0,0 +1,40 @@
+package io.github.dddplus.ast.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class KMeansTest {
+
+    @Test
+    void cluster() {
+        List<String> methodNames = new ArrayList<>(Arrays.asList(
+                "getUserName", "setUserName", "getUserAge", "setUserAge", "getUserEmail", "setUserEmail",
+                "getAccountBalance", "setAccountBalance", "deposit", "withdraw", "transferMoney",
+                "createUser", "deleteUser", "updateUser", "getUserById", "getUsersByAge",
+                "getUserByEmail", "getUsersByBalance", "getTransactionHistory", "generateReport"
+        ));
+        List<double[]> vectors = new ArrayList<>();
+        Map<String, double[]> vectorMap = new HashMap<>();
+
+        // 将方法名转换为向量
+        for (String methodName : methodNames) {
+            double[] vector = new double[methodName.length()];
+            for (int i = 0; i < methodName.length(); i++) {
+                vector[i] = methodName.charAt(i); // 向量值为ascii
+            }
+            vectors.add(vector);
+            vectorMap.put(methodName, vector);
+        }
+
+        // 使用K-Means算法进行聚类
+        KMeans kMeans = new KMeans();
+        List<List<String>> clusters = kMeans.cluster(vectors, methodNames, vectorMap);
+
+        // 输出聚类结果
+        for (int i = 0; i < clusters.size(); i++) {
+            System.out.println("Cluster " + (i + 1) + ": " + clusters.get(i));
+        }
+    }
+
+}
---
+++ dddplus-visualization/src/main/java/io/github/dddplus/ast/algorithm/KMeans.java
@@ -0,0 +1,100 @@
+package io.github.dddplus.ast.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class KMeans {
+    private static final int NUM_CLUSTERS = 3;
+    private static final int MAX_ITERATIONS = 100;
+    private static final double THRESHOLD = 0.001;
+
+    private int numClusters; // 簇的数量
+    private int maxIterations; // 最大迭代次数
+    private double threshold; // 收敛阈值
+
+    public KMeans() {
+        this(NUM_CLUSTERS, MAX_ITERATIONS, THRESHOLD);
+    }
+
+    public KMeans(int numClusters, int maxIterations, double threshold) {
+        this.numClusters = numClusters;
+        this.maxIterations = maxIterations;
+        this.threshold = threshold;
+    }
+
+    public List<List<String>> cluster(List<double[]> vectors, List<String> labels, Map<String, double[]> vectorMap) {
+        // 随机初始化簇中心
+        List<double[]> centroids = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < numClusters; i++) {
+            centroids.add(vectors.get(random.nextInt(vectors.size())));
+        }
+
+        // 迭代聚类
+        List<List<String>> clusters = new ArrayList<>();
+        for (int i = 0; i < maxIterations; i++) {
+            // 分配样本到最近的簇
+            clusters.clear();
+            for (int j = 0; j < numClusters; j++) {
+                clusters.add(new ArrayList<>());
+            }
+            for (int j = 0; j < vectors.size(); j++) {
+                double[] vector = vectors.get(j);
+                double minDistance = Double.MAX_VALUE;
+                int nearestCluster = -1;
+                for (int k = 0; k < numClusters; k++) {
+                    double[] centroid = centroids.get(k);
+                    double distance = euclideanDistance(vector, centroid);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        nearestCluster = k;
+                    }
+                }
+                clusters.get(nearestCluster).add(labels.get(j));
+            }
+
+            // 更新簇中心
+            boolean converged = true;
+            for (int j = 0; j < numClusters; j++) {
+                double[] newCentroid = calculateCentroid(clusters.get(j), vectorMap);
+                double[] oldCentroid = centroids.get(j);
+                if (euclideanDistance(newCentroid, oldCentroid) > threshold) {
+                    centroids.set(j, newCentroid);
+                    converged = false;
+                }
+            }
+
+            // 如果簇中心未发生变化，则结束迭代
+            if (converged) {
+                break;
+            }
+        }
+
+        return clusters;
+    }
+
+    private double[] calculateCentroid(List<String> cluster, Map<String, double[]> vectorMap) {
+        int dimension = vectorMap.values().iterator().next().length;
+        double[] centroid = new double[dimension];
+        for (String label : cluster) {
+            double[] vector = vectorMap.get(label);
+            for (int i = 0; i < dimension; i++) {
+                centroid[i] += vector[i];
+            }
+        }
+        for (int i = 0; i < dimension; i++) {
+            centroid[i] /= cluster.size();
+        }
+        return centroid;
+    }
+
+    private double euclideanDistance(double[] a, double[] b) {
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++) {
+            sum += Math.pow(a[i] - b[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+}
