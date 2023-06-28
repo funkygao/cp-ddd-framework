02211123488d2498bc38d619366841a9b3cf188d
gaopeng71 <gaopeng8@jd.com>
2023-06-28 10:26:19 +0800

    explicit clustering

--- dddplus-visualization/src/main/java/io/github/dddplus/ast/view/PlainTextBuilder.java
+++ dddplus-visualization/src/main/java/io/github/dddplus/ast/view/PlainTextBuilder.java
@@ -16,8 +16,14 @@ public class PlainTextBuilder {
     private static final String TAB = SPACE + SPACE + SPACE;
     private static final String NEWLINE = System.getProperty("line.separator");
 
+    private boolean clustering = false;
     private final StringBuilder content = new StringBuilder();
     private ReverseEngineeringModel model;
+
+    public PlainTextBuilder clustering() {
+        this.clustering = true;
+        return this;
+    }
 
     public PlainTextBuilder build(ReverseEngineeringModel model) {
         this.model = model;
@@ -128,14 +134,16 @@ public class PlainTextBuilder {
                 }
             }
 
-            List<List<String>> clusters = keyModelEntry.methodClusters();
-            if (clusters != null) {
-                append(TAB).append("[聚类]").append(NEWLINE);
-                for (int i = 0; i < clusters.size(); i++) {
-                    if (clusters.get(i).isEmpty()) {
-                        continue;
+            if (clustering) {
+                List<List<String>> clusters = keyModelEntry.methodClusters();
+                if (clusters != null) {
+                    append(TAB).append("[聚类]").append(NEWLINE);
+                    for (int i = 0; i < clusters.size(); i++) {
+                        if (clusters.get(i).isEmpty()) {
+                            continue;
+                        }
+                        append(TAB).append(TAB).append(clusters.get(i).toString()).append(NEWLINE);
                     }
-                    append(TAB).append(TAB).append(clusters.get(i).toString()).append(NEWLINE);
                 }
             }
         }
