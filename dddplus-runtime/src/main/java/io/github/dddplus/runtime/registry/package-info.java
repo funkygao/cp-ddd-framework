92e1f94dfcd68b709f003eb7ad2f05ae8dd77081
Funky Gao <funky.gao@gmail.com>
2023-06-29 00:58:44 +0800

    tweak of package javadoc

--- dddplus-runtime/src/main/java/io/github/dddplus/runtime/registry/package-info.java
+++ dddplus-runtime/src/main/java/io/github/dddplus/runtime/registry/package-info.java
@@ -4,6 +4,6 @@
  * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 /**
- * DDD的组件运行时发现、注册、索引、分发机制.
+ * Internal extension points discovery/dynamic dispatching mechanism.
  */
 package io.github.dddplus.runtime.registry;
