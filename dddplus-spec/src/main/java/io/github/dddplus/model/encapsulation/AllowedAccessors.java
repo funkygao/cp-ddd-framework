7b9a9d809b4112966cc9b5ca0c30efe7d8c8f55b
Funky Gao <funky.gao@gmail.com>
2023-07-13 13:25:21 +0800

    fix dead link

--- dddplus-spec/src/main/java/io/github/dddplus/model/encapsulation/AllowedAccessors.java
+++ dddplus-spec/src/main/java/io/github/dddplus/model/encapsulation/AllowedAccessors.java
@@ -18,7 +18,7 @@ import java.lang.annotation.Target;
  * <p>面向定向类的可访问方法.</p>
  * <p>例如，一个{@code Entity}为了能让{@link IRepository}在构造时传入一些属性，就需要把本来无需{@code public}的方法公开.</p>
  * <p>这破坏了封装，并且可能引发{@code bad side effect}.</p>
- * <p>为了解决这个问题，我们引入{@link AllowedAccessors}，并结合{@code AccessorsEnforcer}通过静态扫描保障.</p>
+ * <p>为了解决这个问题，我们引入{@link AllowedAccessors}，并结合{@code AllowedAccessorsEnforcer}通过静态扫描保障.</p>
  */
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 @Retention(RetentionPolicy.SOURCE)
