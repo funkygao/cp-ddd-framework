72f10f1cb7f1fd0cc29db33c77b6d1c238738359
Funky Gao <funky.gao@gmail.com>
2023-07-18 23:07:10 +0800

    add FAQ of the reverse modeling DSL

--- dddplus-spec/src/main/java/io/github/dddplus/package-info.java
+++ dddplus-spec/src/main/java/io/github/dddplus/package-info.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 /**
- * DDD Enhancement Framework with forward and reverse modeling.
+ * DDD Enhancement Framework with forward and reverse modeling mechanism.
  *
  * <p>empowering extension oriented architecture.</p>
  */
