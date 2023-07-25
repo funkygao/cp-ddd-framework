7d22eac49cc7913ae3e48b58c1211cbb44cf467b
gaopeng71 <gaopeng8@jd.com>
2023-07-25 18:16:01 +0800

    kill scripts: it has nothing to do with DDDplus

--- scripts/building_blocks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#====================================================
-# 根据文件名来统计研发思想里的building block
-#
-# Usage：在某个代码库的根目录执行本脚本
-#====================================================
-
-find . -type f -name "*.java" | grep -v Test.java | xargs basename | cut -d. -f1 | sed 's/\([A-Z]\)/ \1/g' | awk 'NF>1{print $NF}' | sort | uniq -c | sort -n | grep -vw 1
