133a2ac91568845a74b141b53c427c29bf85817f
gaopeng71 <gaopeng8@jd.com>
2023-08-30 15:12:08 +0800

    n

---
+++ dddplus-visualization/src/main/java/io/github/dddplus/bce/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright DDDplus Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+/**
+ * Reverse modeling runtime based on Byte Code Engineering.
+ */
+package io.github.dddplus.bce;
